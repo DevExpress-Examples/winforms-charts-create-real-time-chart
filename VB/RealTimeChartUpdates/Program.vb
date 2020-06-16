7386b36518086874cdafc8c744e8baee2036866d
DevExpressExampleBot <fakeEmail>
2020-06-16 11:41:34 +0300

    Source auto update [skip ci]

---
+++ VB/RealTimeChartUpdates/Program.vb
@@ -0,0 +1,20 @@
+﻿Imports DevExpress.XtraEditors
+Imports System
+Imports System.Windows.Forms
+
+Namespace RealTimeChartUpdates
+	Friend Module Program
+
+		<STAThread>
+		Sub Main()
+			WindowsFormsSettings.SetDPIAware()
+			WindowsFormsSettings.AllowDpiScale = True
+			WindowsFormsSettings.AllowAutoScale = DevExpress.Utils.DefaultBoolean.True
+			WindowsFormsSettings.ForceDirectXPaint()
+			Application.EnableVisualStyles()
+			Application.SetCompatibleTextRenderingDefault(False)
+			Application.Run(New Form1())
+
+		End Sub
+	End Module
+End Namespace
